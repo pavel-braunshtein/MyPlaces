94bd30343a12b7b94106bcfeb047bc1c03ae8b29
pavelbraun333 <pavelbraun333@gmail.com>
2023-10-27 15:48:23 +0300

    feature: changed style,jnmnb,n.j

---
+++ MyPlaces/MainViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Pavel on 27.10.2023.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    
+    let restaurantNames = [
+    "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+    "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+    "Speak Easy", "Morris Pub", "Вкусные истории",
+    "Классик", "Love&Life", "Шок", "Бочка"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+       return cell
+    }
+
+    // MARK: - Table Viev Delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Pavel on 27.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
